4275e9712386bc7931c9c8947597e48bb56ef344
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-07-15 19:14:57 +0500

    chore

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,11 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+
+      <img
+        src="https://cpbkfsuxkwclxynwyhmr.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg"
+        alt="cabin"
+      />
     </Row>
   );
 }
