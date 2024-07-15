c72e8ceb76fc56bed29c6599131ebb2bb83a57bc
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-07-15 18:53:49 +0500

    feat: connected Supabase with the React app

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://cpbkfsuxkwclxynwyhmr.supabase.co";
+
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwYmtmc3V4a3djbHh5bnd5aG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwNDA1ODQsImV4cCI6MjAzNjYxNjU4NH0.AbKZiUbwiCpezttY1fB8FKbbm9TsPb4LJQzToBybcT4`;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
