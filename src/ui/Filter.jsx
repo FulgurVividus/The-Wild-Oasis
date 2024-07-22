109ea7ca88ab5036f739acfa10e786a37e7191fc
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-07-22 22:06:53 +0500

    fix

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -37,7 +37,7 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const currentFilter = searchParams.get(filterField) || options.at(0);
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
 
   function handleClick(value) {
     searchParams.set(filterField, value);
